7f5741f5404e58736d210115466daca8dee5253c
Bretton Simpson <bsimpson@ucsd.edu>
2025-02-28 11:16:11 -0800

    Added UnboundLocalError exception for capture file reading script

--- pynoseti/extract/read_capture_file.py
+++ pynoseti/extract/read_capture_file.py
@@ -12,8 +12,12 @@ def read_capture_file(file):
     except error.Scapy_Exception as e:
         print('\nUnsupported capture file type. Skipping for now.\n')
     
-    for packet in capture:
-        concatenated_batch_data.append(packet)
+    try:
+        for packet in capture:
+            concatenated_batch_data.append(packet)
+
+    except UnboundLocalError as e:
+        print('\nUnbound local error encountered. Continuing to next file.\n')
 
     del capture
     gc.collect()    
