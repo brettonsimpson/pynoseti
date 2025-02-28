5f5f840a4f38233e1f543623be40897f22f381a5
Bretton Simpson <bsimpson@ucsd.edu>
2025-02-28 11:04:26 -0800

    Added data preprocessing script exception for temporarily skipping unsupported capture file types

--- pynoseti/extract/read_capture_file.py
+++ pynoseti/extract/read_capture_file.py
@@ -1,11 +1,16 @@
 import gc
 from scapy.all import rdpcap
+from scapy import error
 
 def read_capture_file(file):
 
     concatenated_batch_data = []
 
-    capture = rdpcap(file)
+    try:
+        capture = rdpcap(file)
+
+    except error.Scapy_Exception as e:
+        print('\nUnsupported capture file type. Skipping for now.\n')
     
     for packet in capture:
         concatenated_batch_data.append(packet)
