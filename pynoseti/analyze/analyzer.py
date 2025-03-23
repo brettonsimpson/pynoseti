f7597337b64e14f345e5fb5477b1c7224bb06e80
Bretton Simpson <bsimpson@ucsd.edu>
2025-03-22 19:01:58 -0700

    Added worker memory usage limit of 5 GB

--- pynoseti/analyze/analyzer.py
+++ pynoseti/analyze/analyzer.py
@@ -17,7 +17,7 @@ from pynoseti.extract.extract_packet_data import convert_unix_time
 
 from pynoseti.process.classes import Source, Source_Candidate
 
-@ray.remote
+@ray.remote(memory=5e9)
 def analyzer_function(file_data):
     events = {
         'Pixel Locations': [],
