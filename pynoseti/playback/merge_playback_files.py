84da3306db1da08b21f51d42f68479192dbbef53
Bretton Simpson <bsimpson@ucsd.edu>
2025-04-02 19:27:15 -0700

    Testing changes for .mp4 playback file merge script

--- pynoseti/playback/merge_playback_files.py
+++ pynoseti/playback/merge_playback_files.py
@@ -1,6 +1,9 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+#from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-def merege_playback_files(directory):
+import ffmpeg
+
+'''
+def merge_playback_files(directory):
 
     for file in directory.glob('*.mp4'):
         file_list = []
@@ -11,3 +14,17 @@ def merege_playback_files(directory):
     final_clip = concatenate_videoclips(clips, method="compose")
     
     final_clip.write_videofile('merged_playback', codec="libx264", audio_codec="aac")
+'''
+
+
+def concatenate_videos(directory):
+    
+    video_list = []
+    for file in directory.glob('*.mp4'):
+        video_list.append(file)
+
+    input_files = [ffmpeg.input(video) for video in video_list]
+    joined = ffmpeg.concat(*input_files, v=1, a=1).output('output_file.mp4', codec='libx264', audio_codec='aac')
+    joined.run()
+
+    print('Done!')
