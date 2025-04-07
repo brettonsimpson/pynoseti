2694754c39379e7845195b76147b0e0ef8d4cb83
Bretton Simpson <bsimpson@ucsd.edu>
2025-04-06 19:50:04 -0700

    Added IndexError exceptions for playback.py

--- pynoseti/playback/playback.py
+++ pynoseti/playback/playback.py
@@ -24,63 +24,75 @@ def playback_function(file, choice, file_name, save_directory):
         
         fig, ax = plt.subplots()
         if 'Ima_onsky' in file_name:
-            im = ax.imshow(array_image_list[telescope_choice].sequence[0].data,
-                           cmap='viridis',
-                           vmin=0,
-                           vmax=array_image_list[telescope_choice].max_count)
-            
-            cbar = fig.colorbar(im, ax=ax, orientation='vertical', label='Photoelectron Count')
 
-            timestamp = ax.set_title(convert_unix_time(array_image_list[telescope_choice].sequence[0].timestamp), loc='left', y=-0.065)
+            try:
 
-            frame_number = ax.text(-0.5, 34.25, 'Frame 1 of '+str(len(array_image_list[telescope_choice].sequence)+1))
+                im = ax.imshow(array_image_list[telescope_choice].sequence[0].data,
+                            cmap='viridis',
+                            vmin=0,
+                            vmax=array_image_list[telescope_choice].max_count)
+                
+                cbar = fig.colorbar(im, ax=ax, orientation='vertical', label='Photoelectron Count')
 
-            telescope_identifier = ax.text(-0.5, 35.5, f'{array_image_list[telescope_choice].telescope}')
+                timestamp = ax.set_title(convert_unix_time(array_image_list[telescope_choice].sequence[0].timestamp), loc='left', y=-0.065)
 
-            logo = mpimg.imread(str(cwd_path)+'/assets/panoseti_logo.png')
+                frame_number = ax.text(-0.5, 34.25, 'Frame 1 of '+str(len(array_image_list[telescope_choice].sequence)+1))
 
-            logo_box = OffsetImage(logo, zoom=0.5)
+                telescope_identifier = ax.text(-0.5, 35.5, f'{array_image_list[telescope_choice].telescope}')
 
-            annotation_box = AnnotationBbox(logo_box, (0.148, 1.081), frameon=False, xycoords='axes fraction')
+                logo = mpimg.imread(str(cwd_path)+'/assets/panoseti_logo.png')
 
-            ax.add_artist(annotation_box)
+                logo_box = OffsetImage(logo, zoom=0.5)
 
-            ax.set_axis_off()
+                annotation_box = AnnotationBbox(logo_box, (0.148, 1.081), frameon=False, xycoords='axes fraction')
 
-            def animate(i):
-                im.set_array(array_image_list[telescope_choice].sequence[i].data)
-                ax.set_title(convert_unix_time(array_image_list[telescope_choice].sequence[i].timestamp), loc='left', y=-0.065)
-                frame_number.set_text(f'Frame {i+1} of '+str(len(array_image_list[telescope_choice].sequence)+1))
-                return [im, ax]
-            
-            playback_period = (array_image_list[0].sequence[len(array_image_list[0].sequence)-1].timestamp-array_image_list[0].sequence[0].timestamp)
-            fps = len(array_image_list[0].sequence)/playback_period
+                ax.add_artist(annotation_box)
+
+                ax.set_axis_off()
+
+                def animate(i):
+                    im.set_array(array_image_list[telescope_choice].sequence[i].data)
+                    ax.set_title(convert_unix_time(array_image_list[telescope_choice].sequence[i].timestamp), loc='left', y=-0.065)
+                    frame_number.set_text(f'Frame {i+1} of '+str(len(array_image_list[telescope_choice].sequence)+1))
+                    return [im, ax]
+                
+                playback_period = (array_image_list[0].sequence[len(array_image_list[0].sequence)-1].timestamp-array_image_list[0].sequence[0].timestamp)
+                fps = len(array_image_list[0].sequence)/playback_period
+
+            except IndexError:
+                pass
 
         else:
-            im = ax.imshow(array_image_list[telescope_choice].sequence[0].data,
-                           cmap='viridis',
-                           vmin=0,
-                           vmax=array_image_list[telescope_choice].max_count)
             
-            cbar = fig.colorbar(im, ax=ax, orientation='vertical', label='Photoelectron Count')
-            title = ax.set_title(convert_unix_time(array_image_list[telescope_choice].sequence[0].timestamp), loc='left', y=-0.065)
-            frame_number = ax.text(-0.5, 34.25, 'Frame 0 of '+str(len(array_image_list[telescope_choice].sequence)))
-
-            telescope_identifier = ax.text(-0.5, 35.5, f'{array_image_list[telescope_choice].telescope}')
-
-            logo = mpimg.imread(str(cwd_path)+'/assets/panoseti_logo.png')
-            logo_box = OffsetImage(logo, zoom=0.5)
-            annotation_box = AnnotationBbox(logo_box, (0.148, 1.081), frameon=False, xycoords='axes fraction')
-            ax.add_artist(annotation_box)
-            ax.set_axis_off()
-
-            def animate(i):
-                im.set_array(array_image_list[telescope_choice].sequence[i].data)
-                ax.set_title(convert_unix_time(array_image_list[telescope_choice].sequence[i].timestamp), loc='left', y=-0.065)
-                frame_number.set_text(f'Frame {i} of '+str(len(array_image_list[telescope_choice].sequence)))
-                return [im, ax]
+            try:
             
-            fps = 20
+                im = ax.imshow(array_image_list[telescope_choice].sequence[0].data,
+                            cmap='viridis',
+                            vmin=0,
+                            vmax=array_image_list[telescope_choice].max_count)
+                
+                cbar = fig.colorbar(im, ax=ax, orientation='vertical', label='Photoelectron Count')
+                title = ax.set_title(convert_unix_time(array_image_list[telescope_choice].sequence[0].timestamp), loc='left', y=-0.065)
+                frame_number = ax.text(-0.5, 34.25, 'Frame 0 of '+str(len(array_image_list[telescope_choice].sequence)))
+
+                telescope_identifier = ax.text(-0.5, 35.5, f'{array_image_list[telescope_choice].telescope}')
+
+                logo = mpimg.imread(str(cwd_path)+'/assets/panoseti_logo.png')
+                logo_box = OffsetImage(logo, zoom=0.5)
+                annotation_box = AnnotationBbox(logo_box, (0.148, 1.081), frameon=False, xycoords='axes fraction')
+                ax.add_artist(annotation_box)
+                ax.set_axis_off()
+
+                def animate(i):
+                    im.set_array(array_image_list[telescope_choice].sequence[i].data)
+                    ax.set_title(convert_unix_time(array_image_list[telescope_choice].sequence[i].timestamp), loc='left', y=-0.065)
+                    frame_number.set_text(f'Frame {i} of '+str(len(array_image_list[telescope_choice].sequence)))
+                    return [im, ax]
+                
+                fps = 20
+
+            except IndexError:
+                pass
             
         movie = animation.FuncAnimation(fig, animate, frames=len(array_image_list[telescope_choice].sequence), interval=100, blit=False)
         print('This might take a while...')
@@ -96,38 +108,39 @@ def playback_function(file, choice, file_name, save_directory):
 
         if 'Ima_onsky' in file_name:
 
-            ims = [ax.imshow(array_image_list[i].sequence[0].data,
-                             animated=True,
-                             vmin=0,
-                             vmax=array_image_list[i].max_count) for i, ax in enumerate(axes)]
-            
-            frame_number = ims[0].axes.text(14, -4.58, 'Frame 1 of '+str(len(array_image_list[0].sequence)+1))
-            logo = mpimg.imread(str(cwd_path)+'/assets/panoseti_logo.png')
-            logo_box = OffsetImage(logo, zoom=0.3)
-            annotation_box = AnnotationBbox(logo_box, (0, 1.15), frameon=False, xycoords='axes fraction')
-            ims[0].axes.add_artist(annotation_box)
+            try:
 
-            for i, ax in enumerate(axes):
-                ax.set_title('PDT '+convert_unix_time(int(array_image_list[i].sequence[0].timestamp)), loc='left')
-                telescope = ax.text(-0.5, 33.5, f'{str(array_image_list[i].telescope)}')
-
-                #if i == 2:
-                #    cbar = fig.colorbar(ims[2], axes[2], orientation='vertical', label='Photoelectron Count')
+                ims = [ax.imshow(array_image_list[i].sequence[0].data,
+                                animated=True,
+                                vmin=0,
+                                vmax=array_image_list[i].max_count) for i, ax in enumerate(axes)]
                 
-                ax.set_axis_off()
+                frame_number = ims[0].axes.text(14, -4.58, 'Frame 1 of '+str(len(array_image_list[0].sequence)+1))
+                logo = mpimg.imread(str(cwd_path)+'/assets/panoseti_logo.png')
+                logo_box = OffsetImage(logo, zoom=0.3)
+                annotation_box = AnnotationBbox(logo_box, (0, 1.15), frameon=False, xycoords='axes fraction')
+                ims[0].axes.add_artist(annotation_box)
 
-            
+                for i, ax in enumerate(axes):
+                    ax.set_title('PDT '+convert_unix_time(int(array_image_list[i].sequence[0].timestamp)), loc='left')
+                    telescope = ax.text(-0.5, 33.5, f'{str(array_image_list[i].telescope)}')
 
-            def animate(frame):
-                for i, im in enumerate(ims):
-                    if frame < len(array_image_list[i].sequence):
-                        im.set_array(np.clip(array_image_list[i].sequence[frame].data, a_min=0, a_max=None))
-                        im.axes.set_title('PDT '+convert_unix_time(int(array_image_list[i].sequence[frame].timestamp)), loc='left')
-                        frame_number.set_text(f'Frame {frame+1} of '+str(len(array_image_list[i].sequence)+1))
-                        
-                return [im]
-            
-            
+                    #if i == 2:
+                    #    cbar = fig.colorbar(ims[2], axes[2], orientation='vertical', label='Photoelectron Count')
+                    
+                    ax.set_axis_off()
+
+                def animate(frame):
+                    for i, im in enumerate(ims):
+                        if frame < len(array_image_list[i].sequence):
+                            im.set_array(np.clip(array_image_list[i].sequence[frame].data, a_min=0, a_max=None))
+                            im.axes.set_title('PDT '+convert_unix_time(int(array_image_list[i].sequence[frame].timestamp)), loc='left')
+                            frame_number.set_text(f'Frame {frame+1} of '+str(len(array_image_list[i].sequence)+1))
+                            
+                    return [im]
+                
+            except IndexError:
+                pass
             
             playback_period = (array_image_list[0].sequence[len(array_image_list[0].sequence)-1].timestamp-array_image_list[0].sequence[0].timestamp)
             fps = len(array_image_list[0].sequence)/playback_period
@@ -138,28 +151,33 @@ def playback_function(file, choice, file_name, save_directory):
             movie.save(f'{save_directory}/{file_name[:-7]}_full_array.mp4', writer='ffmpeg', fps=fps, dpi=60)
 
         else:
-            ims = [ax.imshow(array_image_list[i].sequence[0].data,
-                             animated=True,
-                             vmin=0,
-                             vmax=array_image_list[i].max_count) for i, ax in enumerate(axes)]
-            
-            frame_number = ims[0].axes.text(14, -4.58, 'Frame 1 of '+str(len(array_image_list[0].sequence)+1))
-            logo = mpimg.imread(str(cwd_path)+'/assets/panoseti_logo.png')
-            logo_box = OffsetImage(logo, zoom=0.3)
-            annotation_box = AnnotationBbox(logo_box, (0, 1.15), frameon=False, xycoords='axes fraction')
-            ims[0].axes.add_artist(annotation_box)
 
-            for i, ax in enumerate(axes):
-                ax.set_title('PDT '+convert_unix_time(float(array_image_list[i].sequence[0].timestamp)), loc='left')
-                telescope = ax.text(-0.5, 33.5, f'{str(array_image_list[i].telescope)}')
-                ax.set_axis_off()
+            try:
+                ims = [ax.imshow(array_image_list[i].sequence[0].data,
+                                animated=True,
+                                vmin=0,
+                                vmax=array_image_list[i].max_count) for i, ax in enumerate(axes)]
+                
+                frame_number = ims[0].axes.text(14, -4.58, 'Frame 1 of '+str(len(array_image_list[0].sequence)+1))
+                logo = mpimg.imread(str(cwd_path)+'/assets/panoseti_logo.png')
+                logo_box = OffsetImage(logo, zoom=0.3)
+                annotation_box = AnnotationBbox(logo_box, (0, 1.15), frameon=False, xycoords='axes fraction')
+                ims[0].axes.add_artist(annotation_box)
 
-            def animate(frame):
-                for i, im in enumerate(ims):
-                    im.set_array(np.clip(array_image_list[i].sequence[frame].data, a_min=0, a_max=None))
-                    im.axes.set_title('PDT '+convert_unix_time(float(array_image_list[i].sequence[frame].timestamp)), loc='left')
-                    frame_number.set_text(f'Frame {frame+1} of '+str(len(array_image_list[i].sequence)+1))
-                return [im]
+                for i, ax in enumerate(axes):
+                    ax.set_title('PDT '+convert_unix_time(float(array_image_list[i].sequence[0].timestamp)), loc='left')
+                    telescope = ax.text(-0.5, 33.5, f'{str(array_image_list[i].telescope)}')
+                    ax.set_axis_off()
+
+                def animate(frame):
+                    for i, im in enumerate(ims):
+                        im.set_array(np.clip(array_image_list[i].sequence[frame].data, a_min=0, a_max=None))
+                        im.axes.set_title('PDT '+convert_unix_time(float(array_image_list[i].sequence[frame].timestamp)), loc='left')
+                        frame_number.set_text(f'Frame {frame+1} of '+str(len(array_image_list[i].sequence)+1))
+                    return [im]
+                
+            except IndexError:
+                pass
             
             movie = animation.FuncAnimation(fig, animate, frames=len(array_image_list[0].sequence), interval=100, blit=False)
             print('This might take a while...')
